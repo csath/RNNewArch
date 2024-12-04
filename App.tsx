da08f73c13c6d4e21b57efdfae6f80171b1b3f15
Chanaka Athurugiriya <chanaka.athurugiriya@tonies.com>
2024-12-04 13:10:00 +0100

    chore(storage): add rn-keychain dep

--- App.tsx
+++ App.tsx
@@ -5,13 +5,16 @@
  * @format
  */
 
-import React from 'react';
+import React, { useState } from 'react';
 import {
   Alert,
   Button,
   SafeAreaView,
   ScrollView,
   StatusBar,
+  StyleSheet,
+  Text,
+  TextInput,
   useColorScheme,
   View,
 } from 'react-native';
@@ -21,39 +24,46 @@ import {
   Header,
 } from 'react-native/Libraries/NewAppScreen';
 import * as Keychain from 'react-native-keychain';
+import { Picker } from '@react-native-picker/picker';
 
 function App(): React.JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
+  const [key, setKey] = useState('1234');
+  const [value, setValue] = useState('1111');
+  const [storageType, setStorageType] = useState(Keychain.STORAGE_TYPE.AES_GCM);
 
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
 
   const writeValue = () => {
-    const value = '1234';
-    Keychain.setInternetCredentials('username', 'unused', value, {
+    Keychain.setInternetCredentials(key, 'unused', value, {
       accessible: Keychain.ACCESSIBLE.WHEN_UNLOCKED_THIS_DEVICE_ONLY,
-      storage: Keychain.STORAGE_TYPE.AES_GCM_NO_AUTH,
+      storage: storageType,
     }).then(() => {
-      Alert.alert('Value written', value);
+      Alert.alert(`Value write complete`, JSON.stringify({
+        key,
+        value,
+        storageType,
+      }, null, 2));
     }).catch((error) => {
-      console.error('error: value written', error);
-    }).finally(() => {
-      console.log('Value written finally');
+      Alert.alert('error: value written', error);
     });
   }
 
   const readValue = () => {
-    Keychain.getInternetCredentials('username').then((value) => {
-      if (typeof value !== 'object') {
-        console.log('value', value);
-        return;
+    Keychain.getInternetCredentials(key).then((value) => {
+      if (value === false) {
+        Alert.alert('Value read', 'No value found for key');
+        return
       }
-      Alert.alert('Value read', value.password);
+      Alert.alert(`Value read complete`, JSON.stringify({
+        key,
+        value: value?.password || false,
+        storageType: value?.storage,
+      }, null, 2));
     }).catch((error) => {
-      console.error('error: value written', error);
-    }).finally(() => {
-      console.log('Value read finally');
+      Alert.alert('error: value written', error);
     });
   }
 
@@ -66,11 +76,45 @@ function App(): React.JSX.Element {
       <ScrollView
         contentInsetAdjustmentBehavior="automatic"
         style={backgroundStyle}>
-        <Header />
         <View
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
+          <Text style={styles.inputTitle}>
+            Key:
+          </Text>
+          <TextInput
+            style={styles.input}
+            onChangeText={setKey}
+            value={key}
+          />
+          <Text style={styles.inputTitle}>
+            Value:
+          </Text>
+          <TextInput
+            style={styles.input}
+            onChangeText={setValue}
+            value={value}
+            placeholder="useless placeholder"
+            keyboardType="numeric"
+          />
+          <View style={styles.picker}>
+            <Text style={styles.inputTitle}>
+              Storage type:
+            </Text>
+            <Picker
+              selectedValue={storageType}
+              onValueChange={(itemValue) =>
+                setStorageType(itemValue)
+              }>
+              {Object.values(Keychain.STORAGE_TYPE).map((item) => {
+                return <Picker.Item key={item} label={item} value={item} />
+              }
+              )}
+            </Picker>
+          </View>
+        </View>
+        <View style={styles.buttonContainer}>
           <Button title='Read value' onPress={readValue} />
           <Button title='Write value' onPress={writeValue} />
         </View>
@@ -79,5 +123,28 @@ function App(): React.JSX.Element {
   );
 }
 
+const styles = StyleSheet.create({
+  buttonContainer: {
+    justifyContent: 'space-around',
+    height: 150
+  },
+  picker: {
+    marginBottom: 10
+  },
+  inputTitle: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: 'white',
+    paddingLeft: 10,
+    paddingTop: 10,
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    backgroundColor: 'white',
+  },
+});
 
 export default App;
